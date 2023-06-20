2bc0c1ba4a8793ead93dabe61691073bb7efda33
khaledmo99 <khaled.mofleh707@gmail.com>
2023-06-20 15:29:52 +0200

    first commit

--- router.js
+++ router.js
@@ -3,6 +3,26 @@ const router = express.Router();
 const argon = require("argon2");
 const database = require("./db_client");
 
+router.post("/api/login", (req, res) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+});
+
 router.get("/", (req, res) => {
   res.status(200).json(" Bienvenue chez moi");
 });
@@ -57,5 +77,57 @@ router.post("/user", async (req, res) => {
     res.status(500).json("user not created");
   }
 });
+
+const jwt = require("jsonwebtoken"); // don't forget to import
+
+// ...
+
+const verifyPassword = (req, res) => {
+  argon2
+    .verify(req.user.hashedPassword, req.body.password)
+    .then((isVerified) => {
+      if (isVerified) {
+        const payload = { sub: req.user.id };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+
+        delete req.user.hashedPassword;
+        res.send({ token, user: req.user });
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
+app.post("/api/user", verifyToken, movieHandlers.postMovie);
 
 module.exports = router;
